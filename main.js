3a0ad08ad147626370175e67df32e30c1e585b41
nabeelnazeer13 <nabeel.nazeer13@gmail.com>
2025-09-26 02:37:19 +0200

    first draft with full functionality

---
+++ main.js
@@ -0,0 +1,109 @@
+
+const currenttasks = [];
+const completedtasks = [];
+const taskinput = document.querySelector("#taskin");
+const newtaskbtn = document.getElementById("newtaskbtn");
+const tasklist = document.getElementById("tasks");
+const completedlist = document.getElementById("ctasks");
+const todocount = document.querySelector("#todotasks");
+let count = 0;
+displaytasks();
+displaycompletedtasks();
+document.getElementById("todotasks").textContent = "You have "+ count + " tasks completed. Only "+ currenttasks.length + " remains";
+newtaskbtn.addEventListener("click", addtask);
+
+
+function addtask()
+{
+    const task = taskinput.value;
+    if (task == ""){
+        alert("Please enter a task");
+    }
+    else {
+        currenttasks.push({ item: task, status: "todo" });
+        taskinput.value = "";
+        displaytasks();
+        displaycompletedtasks();
+        document.getElementById("todotasks").textContent = "You have "+ count + " tasks completed. Only "+ currenttasks.length + " remains";
+    }}
+
+function displaytasks(){
+tasklist.innerHTML = "";
+for (let i = 0; i < currenttasks.length; i++){
+    const li = document.createElement("li");
+    const text = document.createElement("span");
+    const del = document.createElement("span");
+    text.textContent = currenttasks[i].item;;
+    del.innerHTML = '&#128465;';
+    del.className = "trashicon";
+    li.appendChild(text);
+    li.appendChild(del);
+    tasklist.appendChild(li);
+    text.addEventListener("click", function(e){    
+    const tsk=e.target;
+    complete(tsk, i)})
+    del.addEventListener("click", function(e){
+        delfunc("current", i);
+    } )
+}}
+
+function complete(tsk, i){
+        if (currenttasks[i].status="todo"){
+            currenttasks[i].status = "completed";
+            count++;
+            let cmpttsk = currenttasks.splice(i, 1)[0];
+            completedtasks.push(cmpttsk);
+            document.getElementById("todotasks").textContent = "You have "+ count + " tasks completed. Only "+ currenttasks.length + " remains";
+            completedlist.append();
+            displaycompletedtasks();
+            displaytasks();
+            
+        }}          
+    
+function displaycompletedtasks(){
+    completedlist.innerHTML = ""
+    for (let i = 0; i < completedtasks.length; i++){
+        const li = document.createElement("li");
+        const text = document.createElement("span");
+        const del = document.createElement("span");
+        text.textContent = completedtasks[i].item;
+        del.innerHTML = '&#128465;';
+        del.className = "trashicon";
+        console.log(del);
+        li.appendChild(text);
+        li.appendChild(del);
+        completedlist.appendChild(li);
+        text.addEventListener("click", function(e){    
+        redo(i)})
+        del.addEventListener("click", function(e){
+            delfunc("complete", i);
+    } )
+        }}
+
+function redo(i){
+    if (completedtasks[i].status==="completed"){
+            completedtasks[i].status = "todo";
+            count--;
+            let cmpttsk = completedtasks.splice(i, 1)[0];
+            currenttasks.push(cmpttsk);
+
+            document.getElementById("todotasks").textContent = "You have "+ count + " tasks completed. Only "+ currenttasks.length + " remains";
+            displaytasks();
+            displaycompletedtasks();}}
+   
+
+function delfunc(listid, i){
+if (listid === "current"){
+    currenttasks.splice(i, 1)[0];
+    displaycompletedtasks();
+    displaytasks();
+}
+else {
+    completedtasks.splice(i, 1) [0];
+    displaycompletedtasks();
+    displaytasks();
+}
+}
+   
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+MY FIRST TO DO APP
+
+En webbapplikation för ToDo som innehåller HTML, CSS och JavaScript uppdelade i separata filer.
+
+Man kan 
+
+1. skriva in en uppgift i ett <input>-fält
+2. Klicka på en knapp för att lägga till uppgiften i en lista som en <li>
+3. klickar på knappen när fältet är tomt ska man få ett meddelande som säger att man måste skriva något.
+4. Klicka på en uppgift <li> i listan för att markera den som klar 
+5. Titta hur många är klara
+6. Uppgifterna som syns i HTML är i synk med en array med objekt i JavaScript, dvs status om klar/inte klar samt att tas bort från array när den tas bort ur listan.
+7. Klicka på en <li> som markerats som färdig igen för att markera den som ofärdig
+8. Klicka på en liten papperskorg på varje rad för att radera den uppgiften ur listan.
+
